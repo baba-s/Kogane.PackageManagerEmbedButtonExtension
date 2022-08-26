0b3f57b01cd2d14ac9e4e8b3ebde7a152b9e5c21
baba-s <shotababa1206@gmail.com>
2022-08-26 19:36:14 +0900

    fix

--- Editor/PackageManagerEmbedButtonExtension.cs
+++ Editor/PackageManagerEmbedButtonExtension.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.UI;
-using UnityEditorInternal;
 using UnityEngine.UIElements;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
@@ -47,7 +42,7 @@ namespace Kogane.Internal
 
             VisualElement root = this;
 
-            while ( root != null && root.parent != null )
+            while ( root is { parent: { } } )
             {
                 root = root.parent;
             }
@@ -57,8 +52,8 @@ namespace Kogane.Internal
                 text = "Embed",
             };
 
-            var removeButton = FindElement( root, x => x.name == "PackageRemoveCustomButton" );
-            removeButton.parent.Add( m_embedButton );
+            var removeButton = root.FindElement( x => x.name == "PackageRemoveCustomButton" );
+            removeButton.parent.Insert( 0, m_embedButton );
 
             m_isInitialized = true;
         }
@@ -69,24 +64,6 @@ namespace Kogane.Internal
 
         void IPackageManagerExtension.OnPackageRemoved( PackageInfo packageInfo )
         {
-        }
-
-        private static VisualElement FindElement
-        (
-            VisualElement             element,
-            Func<VisualElement, bool> predicate
-        )
-        {
-            var selected                    = new List<VisualElement>();
-            var engineAssemblyPath          = InternalEditorUtility.GetEngineAssemblyPath();
-            var engineAssemblyDirectoryName = Path.GetDirectoryName( engineAssemblyPath ).Replace( "\\", "/" );
-            var assembly                    = Assembly.LoadFile( $@"{engineAssemblyDirectoryName}\UnityEditor.UIBuilderModule.dll" );
-            var type                        = assembly.GetType( "Unity.UI.Builder.VisualElementExtensions" );
-            var findElementsRecursiveMethod = type.GetMethod( "FindElementsRecursive", BindingFlags.Static | BindingFlags.NonPublic );
-
-            findElementsRecursiveMethod.Invoke( null, new object[] { element, predicate, selected } );
-
-            return selected.Count == 0 ? null : selected[ 0 ];
         }
     }
 }
